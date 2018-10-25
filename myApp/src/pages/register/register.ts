ba54fb2242a7bf981acddb18a70f2cc3006a0599
xinyigu2 <Sunny@wirelessprv-10-194-216-51.near.illinois.edu>
2018-10-24 19:58:23 -0500

    done

---
+++ myApp/src/pages/register/register.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+  username:string;
+  password:string;
+  repass:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+  register(){
+    if(this.username.length==0 || this.password.length ==0 || this.password.length ==0){
+      alert("please fill all fields");
+    }
+  }
+}
